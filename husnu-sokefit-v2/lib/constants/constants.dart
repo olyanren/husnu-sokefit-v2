ee8abab392dc19dad7d3e37544d5b2f371a56054
Muhammed YC <olyanren@gmail.com>
2021-07-02 14:44:58 +0300

    sökefit tamamlandı

--- husnu-sokefit-v2/lib/constants/constants.dart
+++ husnu-sokefit-v2/lib/constants/constants.dart
@@ -10,7 +10,7 @@ class Constants {
 
   static const String ONESIGNAL_APP_ID ='2455960d-da2e-4737-94b0-1135e5d0caaa';
 
-  static const String BASE_PATH = "http://api2.sokefit.com";
+  static const String BASE_PATH = "https://api.sokefit.com";
   //  static const String BASE_PATH = "http://10.0.2.2:8000";
   static const String API_PATH = "/api/v1";
 }
